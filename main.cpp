8c9d9cacf9788ddc9ffc45caa5e2bc7abb42b09f
Matvey <89075498+herobrine1337@users.noreply.github.com>
2024-01-27 19:30:26 +0300

    works only with windows cmd

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ const std::wstring PIXEL_BRIGHTNESS = L"@#%&?*+;:^~-_,. ";
 const std::wstring PIXEL_COLORS_PREFIX = L"\x1b[38;2;";
 
 std::vector<int> GetColorAmount(const int& beginRow, const int& stepRow,
-                                 const int& beginCol, const int& stepCol, const cv::Mat& frame){
+                                const int& beginCol, const int& stepCol, const cv::Mat& frame){
     auto sum = std::vector<int>{0,0,0};
     for(int r = beginRow; r < beginRow+stepRow; r++){
         for(int c = beginCol; c < beginCol+stepCol; c++){
@@ -38,18 +38,18 @@ std::wstring ConvertPixels(cv::Mat& frame, const int cols, const int lines){
     for(int l = 0; l <= frameLines-frameLines/lines; l += frameLines/lines){
         for(int c = 0; c <= frameCols-frameCols/cols; c += frameCols/cols){
             auto colors = GetColorAmount(l, frameLines/lines,
-                                          c, frameCols/cols, frame);
+                                         c, frameCols/cols, frame);
             int colorSum = colors[0] + colors[1] + colors[2];
             colorPrefix = PIXEL_COLORS_PREFIX + std::to_wstring(colors[2]/(totalUnity*32) *32)
-                    + L";" + std::to_wstring(colors[1]/(totalUnity*32) *32)
-                    + L";" + std::to_wstring(colors[0]/(totalUnity*32) *32) + L"m";
+                          + L";" + std::to_wstring(colors[1]/(totalUnity*32) *32)
+                          + L";" + std::to_wstring(colors[0]/(totalUnity*32) *32) + L"m";
             if(colorPrefix == prevColorPrefix){
                 colorPrefix = L"";
             } else{
                 prevColorPrefix = colorPrefix;
             }
             ResultedFrame.append(colorPrefix + PIXEL_BRIGHTNESS
-                    [PIXEL_BRIGHTNESS.size()-1-(colorSum*PIXEL_BRIGHTNESS.size())/(totalUnity*256*3)]);
+            [PIXEL_BRIGHTNESS.size()-1-(colorSum*PIXEL_BRIGHTNESS.size())/(totalUnity*256*3)]);
         }
         ResultedFrame += L"\n";
     }
@@ -57,18 +57,28 @@ std::wstring ConvertPixels(cv::Mat& frame, const int cols, const int lines){
 }
 
 int main(int argc, char* argv[]){
-    if(argc != 4){
+    if(argc != 2){
         std::cerr << "Error getting parameters" << std::endl;
         return -1;
     }
 
-    cv::VideoCapture video(argv[3]);
+    cv::VideoCapture video(argv[1]);
     if (!video.isOpened()) {
         std::cerr << "Error opening video file." << std::endl;
         return -1;
     }
 
-    // Set output mode to handle virtual terminal sequences
+
+    system("cls");
+    cv::waitKey(3000) == 13;
+    std::cout << "Loading...";
+
+
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    const int LINES = 9*(csbi.srWindow.Bottom - csbi.srWindow.Top)/10;
+    const int COLS = 9*(csbi.srWindow.Right - csbi.srWindow.Left)/10;
+
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
     if (hOut == INVALID_HANDLE_VALUE)
     {
@@ -90,23 +100,22 @@ int main(int argc, char* argv[]){
     std::wcout.tie(NULL);
 
     const int VIDEO_FPS = video.get(cv::CAP_PROP_FPS);
-    const int COLS = atoi(argv[1]), LINES = atoi(argv[2]);
 
     std::vector<std::wstring> result;
     cv::Mat frame;
     while(video.read(frame)){
         result.push_back(ConvertPixels(frame, COLS, LINES));
     }
+    system("cls");
     video.release();
     for(auto& asciiFrames: result){
-        std::wcout << asciiFrames;
+        std::wcout << asciiFrames << std::flush;
 
-        if (cv::waitKey(500/VIDEO_FPS) == 'q') {
+        if (cv::waitKey(1000/VIDEO_FPS) == 'q') {
             std::wcout << L"\x1b[39m";
             return 0;
         }
     }
     std::wcout << L"\x1b[39m";
-    system("pause");
     return 0;
 }
